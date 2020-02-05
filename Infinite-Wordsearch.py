776941f187970b8e331759bfd0c0cea151480497
PowerForSeconds <powerforseconds@gmail.com>
2020-02-04 18:41:33 -0800

    Added movement and sleeping

--- Infinite-Wordsearch.py
+++ Infinite-Wordsearch.py
@@ -1,4 +1,4 @@
-import random, os
+import random, os, time
 os.system("cls")
 
 def clearscreen():
@@ -45,5 +45,16 @@ while True:
         for x in range(10):
             seed = round(((location[0] + x + 0.1) / (location[1] + y + 0.1)) * 93452647) * 7691524
             random.seed(seed)
-            print(characters[random.randint(0, 25)], end="")
+            print(characters[random.randint(0, 25)], end="", flush=True)
+            time.sleep(0.001)
         print()
+    todo = input()
+    if todo == "d":
+        location[0] += 1
+    if todo == "a":
+        location[0] -= 1
+    if todo == "s":
+        location[1] += 1
+    if todo == "w":
+        location[1] -= 1
+    
